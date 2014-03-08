cc6c23eca7b5b633597656738437c20f6339b9e8
Headkiller <patrice_pedneault@live.ca>
2014-03-07 22:54:40 -0500

    -Wave base

---
+++ Assets/Script/WaveCollision.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaveCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) 
+	{	
+		if(collision.gameObject.tag == "Terrain") 
+		{		
+			Debug.Log("Collision Terrain");
+		}
+
+		if(collision.gameObject.tag == "Player") 
+		{		
+			Debug.Log("Collision Player");
+		}
+
+		if(collision.gameObject.tag == "PlayerWeapon") 
+		{		
+			Debug.Log("Collision Player Weapon");
+		}
+	}
+}
---
+++ Assets/Script/WaveScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaveScript : MonoBehaviour {
+
+	float waveSpeed = -3f;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		
+		//gameObject.rigidbody2D.AddForce(new Vector2(waveSpeed, 0f));
+		
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{	
+		gameObject.rigidbody2D.velocity = new Vector2 (waveSpeed, 0f);
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) 
+	{				
+		if(collision.gameObject.tag == "Terrain") 
+		{
+			Debug.Log("Collision Terrain");
+		}
+	}
+}
