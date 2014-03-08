8ae771f0dbb5f3dfe4d1c8e12d5feaa95f3478de
werewolf145 <samuel.grenon145@gmail.com>
2014-03-07 23:01:45 -0500

    Revert "Revert "Ajout du script du jump.""
    This reverts commit 482645ca19a1ff9963e20b8d7e076ae9adaa54dd.

---
+++ Assets/Script/Jump.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour {
+
+	public bool jump = false;
+
+	public float jumpForce = 5.0f;
+
+	private Transform groundDetector;
+	private bool grounded = false;
+	private bool isFalling = false;
+
+	void Awake()
+	{
+		groundDetector = transform.Find("GroundDetector");
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		grounded = Physics2D.Linecast(transform.position, groundDetector.position, 1 << LayerMask.NameToLayer("Terrain"));//Ligne qui check si on a touché le sol.
+
+		if (Input.GetButtonDown("Jump") && grounded) 
+		{
+			jump = true;
+		}
+
+		if(rigidbody2D.velocity.y < 0)
+		{
+			isFalling = true;
+		}
+		else
+		{
+			isFalling = false;
+		}
+	}
+
+	void FixedUpdate()
+	{
+		if (jump)//Si on peut sauter. 
+		{
+			rigidbody2D.velocity = new Vector2(0, jumpForce);
+			jump = false;
+		}
+	}
+}
