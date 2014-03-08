76416926e30897c6828368ac0d0795a9a8f85abc
werewolf145 <samuel.grenon145@gmail.com>
2014-03-08 01:20:58 -0500

    Ajout du script qui gère les score.

---
+++ Assets/Script/Score.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour 
+{
+	private int score;
+	public Font myFont;
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (score > 0) 
+		{
+			score -= 30;
+		}
+	}
+
+	void inscreseScore(int newScore)
+	{
+		score += newScore;
+	}
+
+	void decreaseScore(int newScore)
+	{
+		if (score - newScore > 0) 
+		{
+			score -= newScore;
+		}
+	}
+
+	void OnGUI()
+	{
+		GUI.skin.font = myFont;
+		GUI.Label(new Rect(0,0,Screen.width,Screen.height), "Subscribers: " + score);
+	}
+}
