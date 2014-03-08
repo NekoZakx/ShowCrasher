4c451925f9c0476e15dbde490d506e32d5f17b1a
Headkiller <patrice_pedneault@live.ca>
2014-03-08 00:13:53 -0500

    -WAveAnim

--- Assets/Script/WaveScript.cs
+++ Assets/Script/WaveScript.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 public class WaveScript : MonoBehaviour {
 
-	float waveSpeed = -3f;
+	private float waveSpeed = -3f;
+	private bool playerCollision = false;
+	private bool playerWeaponCollision = false;
 	
 	// Use this for initialization
 	//lol
@@ -14,13 +16,11 @@ public class WaveScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{	
-		Debug.Log ("test");
 		rigidbody2D.velocity = new Vector2 (waveSpeed, 0f);
 	}
 	
 	void OnCollisionEnter2D(Collision2D collision) 
 	{	
-
 		if(collision.gameObject.tag == "Terrain") 
 		{		
 			Debug.Log("Collision Terrain");
@@ -29,11 +29,13 @@ public class WaveScript : MonoBehaviour {
 		if(collision.gameObject.tag == "Player") 
 		{		
 			Debug.Log("Collision Player");
+			playerCollision = true;
 		}
 		
 		if(collision.gameObject.tag == "PlayerWeapon") 
 		{		
 			Debug.Log("Collision Player Weapon");
+			playerWeaponCollision = true;
 		}
 	}
 
