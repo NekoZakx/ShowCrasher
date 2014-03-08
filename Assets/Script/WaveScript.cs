abe75a60335b3ccd07f3a5b50ec3df69a99598e4
Headkiller <patrice_pedneault@live.ca>
2014-03-07 23:38:26 -0500

    -Wave Base

--- Assets/Script/WaveScript.cs
+++ Assets/Script/WaveScript.cs
@@ -8,25 +8,33 @@ public class WaveScript : MonoBehaviour {
 	// Use this for initialization
 	//lol
 	void Start () 
-	{
-		
-		//gameObject.rigidbody2D.AddForce(new Vector2(waveSpeed, 0f));
-		
-
-
+	{	
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{	
-		gameObject.rigidbody2D.velocity = new Vector2 (waveSpeed, 0f);
+		Debug.Log ("test");
+		rigidbody2D.velocity = new Vector2 (waveSpeed, 0f);
 	}
-
+	
 	void OnCollisionEnter2D(Collision2D collision) 
-	{				
+	{	
+
 		if(collision.gameObject.tag == "Terrain") 
-		{
+		{		
 			Debug.Log("Collision Terrain");
 		}
+		
+		if(collision.gameObject.tag == "Player") 
+		{		
+			Debug.Log("Collision Player");
+		}
+		
+		if(collision.gameObject.tag == "PlayerWeapon") 
+		{		
+			Debug.Log("Collision Player Weapon");
+		}
 	}
+
 }
