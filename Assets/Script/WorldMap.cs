bdff6fd6c9ac55e08770506417bac139c7de371e
Zack Niwa <zack.niwa@gmail.com>
2014-03-09 14:45:26 -0400

    First Screen map optimized to fit the screen and Readme for the GameJam!

--- Assets/Script/WorldMap.cs
+++ Assets/Script/WorldMap.cs
@@ -5,10 +5,11 @@ public class WorldMap : MonoBehaviour {
 
 	private static string textToEdit = "";
 	public Font myFont; 
+	public Texture2D bg;
 
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
@@ -18,13 +19,15 @@ public class WorldMap : MonoBehaviour {
 
 	void OnGUI() 
 	{
+		GUI.DrawTexture(new Rect(0,0,Screen.width, Screen.height),bg);
+
 		GUI.skin.font = myFont;
-		if (GUI.Button(new Rect(10, 100, 200, 40), "Begin Tour"))
+		if (GUI.Button(new Rect(Screen.width/33 * 11.7f, ((Screen.height/18)*6.8f), Screen.width/12, Screen.height/18), ""))
 		{
 			Application.LoadLevel("Menu");			
 		}
-		GUI.Label(new Rect(10,0,Screen.width,Screen.height), "Path of your song: ");
-		textToEdit = GUI.TextField(new Rect(10, 50, 200, 30), textToEdit, 25);
+		GUI.Label(new Rect(10,Screen.height - 70,Screen.width,Screen.height), "<color=black>Path to your sound:</color>");
+		textToEdit = GUI.TextField(new Rect(10, Screen.height - 40, 200, 30), textToEdit, 25);
 		GlobalVariable.songPath = textToEdit;
 	} 
 }
---
+++ README.md
@@ -0,0 +1,25 @@
+ShowCrasher
+===========
+
+ShowCrasher est un jeu de plateforme rythmique où l'on doit détruire la musique d'un band qui joue afin de faire accroitre les vues de votre profils de stream(Youtube, Twich, etc... à votre choix, laissé votre imagination décider) et pourrir la vie a ses chanteurs fictivement bien sûr! :) 
+
+## Éxécution
+
+Pour éxécuter le jeu
+
+	Éxécuter le fichier ShowCrasher.exe dans le dossier ShowCrasher à la racine du Git
+	
+## Prérequis
+
+* Un ordinateur
+* Un clavier avec les touches Espace et Entré de fonctionnels
+
+## Compilation
+Utilisez [Unity](http://unity3d.com/unity)
+
+## Équipe
+Samuel Grenon (Programmeur)
+Patrice Pedneault (Programmeur)
+Ian Lafontaine (Artiste) qui a fait un long voyage pour le GameJam et on le remerci grandement!
+Pier-Marc Boulianne (Programmeur)
+Maxime Privé (Programmeur)
