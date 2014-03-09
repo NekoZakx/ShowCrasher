3a74cc7003da1d770e346ebb7615716cd616b619
Zack Niwa <zack.niwa@gmail.com>
2014-03-08 20:45:17 -0500

    full of stuff

--- Assets/Script/MusicLoader.cs
+++ Assets/Script/MusicLoader.cs
@@ -4,17 +4,34 @@ using System.Collections;
 [RequireComponent(typeof(AudioSource))]
 public class MusicLoader : MonoBehaviour {
 
-	AudioSource src;
+	public AudioSource GameMusic;
+	private AudioSource crowd;
+	private float timeBeforeLoadMusic;
+	private float timeLeft = 0f;
 
 	// Use this for initialization
 	void Start () {
-		src = GetComponent<AudioSource>();
-		src.clip = Resources.Load<AudioClip>("Queen - We Are The Champion");
-		src.Play();
+		GameMusic = GetComponent<AudioSource>();
+		Debug.LogWarning("SIZE OF MAIN MUSIC: " + GameMusic.clip.length);
+		//GameMusic.clip = Resources.Load<AudioClip>("Queen - We Are The Champion");
+
+		crowd = GameObject.FindGameObjectWithTag("CrowdSound").GetComponent<AudioSource>();
+		crowd.clip = Resources.Load<AudioClip>("cheerring");
+		timeBeforeLoadMusic = crowd.clip.length;
+		crowd.Play();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		timeLeft += Time.deltaTime;
+
+		if(timeLeft >= timeBeforeLoadMusic/2.1 && !crowd.loop)
+		{
+			crowd.clip = Resources.Load<AudioClip>("CrowdHappy");
+			crowd.loop = true;
+			crowd.Play();
+			GameMusic.Play();
+			GameObject.FindGameObjectWithTag("Respawn").GetComponent<WaveSpawn>().ProduceWave = true;
+		}
 	}
 }
